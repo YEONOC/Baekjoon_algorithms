bd3f9a709682e38bbd6f5d8e83828bb0f6bb4c5a
최보석 <boseok@choeboseog-ui-MacBookPro.local>
2021-11-19 20:08:07 +0900

    추가 : 25829_HASHING

---
+++ 15829_HASHING/15829.py
@@ -0,0 +1,14 @@
+l = int(input())
+s = input()
+s_i = list()
+answer = 0
+
+for i in range(l) :
+    s_i.append(ord(s[i]) - ord('a') + 1)
+
+for i in range(l) :
+    answer += s_i[i] * (31**i)
+
+answer = answer % 1234567891
+
+print(answer)
