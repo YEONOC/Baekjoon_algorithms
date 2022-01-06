3a611bb211a4cc724f62291cbc5fd13603390a41
최보석 <boseok@choeboseog-ui-MacBookPro.local>
2022-01-06 15:05:22 +0900

    추가 : 2164_카드2

---
+++ 2164_카드2/2164.py
@@ -0,0 +1,34 @@
+'''
+#리스트 이용 (시간초과)
+import sys
+n = int(sys.stdin.readline())
+
+que = []
+
+for i in range(1, n+1) :
+    que.append(i)
+
+while len(que) != 1:
+    i = 0
+    que.pop(i)
+    move=que[0]
+    for j in range(1, len(que)) :
+        que[j-1] = que[j]
+    que[len(que)-1] = move
+
+print(que)
+'''
+
+#deque이용 (성공)
+from collections import deque
+
+d = deque()
+
+for i in range(int(input())) :
+    d.append(i+1)
+
+while(len(d) != 1) :
+    d.popleft()
+    d.rotate(-1)
+
+print(*d)
